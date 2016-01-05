ce1dd574e6ab04b0ea83a5c31467ae88d809afd7
telly <teliwal91@gmail.com>
2016-01-05 10:15:11 +0100

    maj

---
+++ src/serveur/element/PersonnageBasique.java
@@ -0,0 +1,60 @@
+/**
+ * 
+ */
+package serveur.element;
+
+import java.util.HashMap;
+
+import utilitaires.Calculs;
+
+/**
+ * Un personnage: un element possedant des caracteristiques et etant capable
+ * de jouer une strategie.
+ * 
+ */
+public class PersonnageBasique extends Personnage {
+	
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Cree un personnage avec un nom et un groupe.
+	 * @param nom du personnage
+	 * @param groupe d'etudiants du personnage
+	 * @param caracts caracteristiques du personnage
+	 */
+	public PersonnageBasique(String nom, String groupe, HashMap<Caracteristique, Integer> caracts) {
+		super(nom, groupe, caracts);
+	}
+	
+	/**
+	 * Incremente la caracteristique donnee de la valeur donnee.
+	 * Si la caracteristique n'existe pas, elle sera cree avec la valeur 
+	 * donnee.
+	 * @param c caracteristique
+	 * @param inc increment (peut etre positif ou negatif)
+	 */
+	public void incrementeCaract(Caracteristique c, int inc) {		
+		if(caracts.containsKey(c)) {
+			caracts.put(c, Calculs.restreintCarac(c, caracts.get(c) + inc));
+		} else {
+			caracts.put(c, Calculs.restreintCarac(c, inc));
+		}
+	}
+	
+	/**
+	 * Tue ce personnage en mettant son nombre de poins de vie a 0.
+	 */
+	public void tue() {
+		caracts.put(Caracteristique.VIE, 0);
+	}
+
+	/**
+	 * Teste si le personnage est vivant, i.e., son nombre de points de vie
+	 * est strictement superieur a 0.
+	 * @return vrai si le personnage est vivant, faux sinon
+	 */
+	public boolean estVivant() {
+		Integer vie = caracts.get(Caracteristique.VIE);
+		return vie != null && vie > 0;
+	}
+}
---
+++ src/serveur/element/PersonnageDjinnarou.java
@@ -0,0 +1,17 @@
+package serveur.element;
+
+import java.util.HashMap;
+
+public class PersonnageDjinnarou extends Personnage {
+
+	public PersonnageDjinnarou(String nom, String groupe, HashMap<Caracteristique, Integer> caracts) {
+		super(nom, groupe, caracts);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
